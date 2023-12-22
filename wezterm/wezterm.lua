16cd86ce3128d672edd19e4e6b6b43096677c97e
Xavier Oswald <xoswald@gmail.com>
2023-12-22 21:02:05 +0100

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,34 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- config.font = wezterm.font 'JetBrains Mono'
-config.font = wezterm.font("Menlo")
+wezterm.on("update-right-status", function(window, pane)
+	local date = wezterm.strftime("%a %b %-d    %H:%M   ")
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+	end
+	window:set_right_status(wezterm.format({
+		{ Text = bat .. "     " .. date },
+	}))
+end)
 
-config.color_scheme = "carbonfox"
+config.font = wezterm.font("JetBrains Mono")
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+-- config.font = wezterm.font("Menlo")
+-- config.color_scheme = "Monokai Soda"
+-- config.color_scheme = "carbonfox"
+-- config.color_scheme = "Dark+"
+-- config.color_scheme = "Catppuccin Mocha (Gogh)"
+-- config.color_scheme = "Calamity"
 
+config.colors = {
+	background = "#151515",
+}
+
+-- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
+config.text_background_opacity = 0.5
 
 config.keys = {
 	-- Let's get some decent keys (Macos Swiss keyboard)
@@ -34,6 +55,8 @@ config.keys = {
 	{ key = "UpArrow", mods = "CMD", action = wezterm.action({ AdjustPaneSize = { "Up", 1 } }) },
 	{ key = "DownArrow", mods = "CMD", action = wezterm.action({ AdjustPaneSize = { "Down", 1 } }) },
 	{ key = "m", mods = "CMD", action = wezterm.action.TogglePaneZoomState },
+
+	-- Ctrl + Shift + X -> Copy mode
 }
 
 config.hide_tab_bar_if_only_one_tab = false
@@ -41,9 +64,10 @@ config.show_tab_index_in_tab_bar = true
 config.show_new_tab_button_in_tab_bar = true
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = true
+config.enable_tab_bar = true
 
 config.font_size = 14
-config.line_height = 1.0
+config.line_height = 0.95
 config.enable_scroll_bar = false
 config.window_close_confirmation = "AlwaysPrompt"
 config.audible_bell = "Disabled"
@@ -54,5 +78,4 @@ config.window_padding = {
 	bottom = 0,
 }
 
--- and finally, return the configuration to wezterm
 return config
